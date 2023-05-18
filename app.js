285656a9fffbdeb195af2e46306b4777114ce014
Transcendence <baydindenys@ukr.net>
2023-05-18 22:08:23 +0300

    upd
    upd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const mongoose = require("mongoose")
-const DB_HOST = "mongodb+srv://baydindenys:iRAMLfPVZ0GFzMih@transcedence.qm6y54a.mongodb.net/?retryWrites=true&w=majority"
+const DB_HOST = "mongodb+srv://baydindenys:iRAMLfPVZ0GFzMih@transcedence.qm6y54a.mongodb.net/db-contacts?retryWrites=true&w=majority"
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
@@ -15,7 +15,14 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-mongoose.connect()
+mongoose.connect(DB_HOST)
+.then(() => {
+// app.listen(3000)
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+})
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
