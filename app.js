7b19ad43b68ea74efb95c9eb4cc689edf4aaf586
Transcendence <baydindenys@ukr.net>
2023-05-22 12:23:25 +0300

    upd

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-const mongoose = require("mongoose")
-// const {DB_HOST} = process.env;
-const DB_HOST = "mongodb+srv://baydindenys:iRAMLfPVZ0GFzMih@transcedence.qm6y54a.mongodb.net/?retryWrites=true&w=majority";
+require("dotenv").config();
+
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
 
 app.use(logger(formatsLogger))
 app.use(cors())
@@ -16,16 +16,7 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-mongoose.set('strictQuery', true)
 
-mongoose.connect(DB_HOST)
-.then(() => {
-console.log("Database connection successful");
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
-})
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
+const mongoose = require("mongoose")
+const {DB_HOST} = process.env;
+// const DB_HOST = "mongodb+srv://baydindenys:iRAMLfPVZ0GFzMih@transcedence.qm6y54a.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB_HOST)
+.then(() => {
+app.listen(3000)
+console.log("Database connection successful");
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
 })
