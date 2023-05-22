a21df99c96b56a42c0d16a95a5fb53a761741999
Transcendence <baydindenys@ukr.net>
2023-05-22 12:25:18 +0300

    package lock del

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const app = require('./app')
 const mongoose = require("mongoose")
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 // const DB_HOST = "mongodb+srv://baydindenys:iRAMLfPVZ0GFzMih@transcedence.qm6y54a.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', true)
 
 mongoose.connect(DB_HOST)
 .then(() => {
-app.listen(3000)
+app.listen(PORT)
 console.log("Database connection successful");
 })
 .catch(error => {
