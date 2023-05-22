cbec1c81ddcc8feb596e838421c5fc7556f4b044
Transcendence <baydindenys@ukr.net>
2023-05-22 12:37:02 +0300

    dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-const dotenv = require('dotenv');
+require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts')
 
@@ -9,15 +9,11 @@ const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-dotenv.config();
-
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
-
-
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
