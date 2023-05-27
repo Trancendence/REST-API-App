b2ddfeaba090a3c71f63e30a882c9437f5286118
Transcendence <baydindenys@ukr.net>
2023-05-27 21:16:18 +0300

    password hash
    password hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,20 @@
+const bcrypt = require("bcrypt");
 const {User} = require("../models/user");
-const { ctrlWrapper } = require('../helpers');
+const { ctrlWrapper, HttpError } = require('../helpers');
 
 const register = async(req, res)=> {
-    const newUser = await User.create(req.body);
+    const {email, password} = req.body;
+    const user = await User.findOne({email}); 
 
-    res.json({
+    if (user) {
+        throw HttpError(409, "Email already in use");
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({...req.body, password: hashPassword});
+
+    res.status(201).json({
         email: newUser.email,
         name: newUser.name,
     })
