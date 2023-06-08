024d179d29da218dad2f9df632041f94fac78bcc
Transcendence <baydindenys@ukr.net>
2023-06-08 22:04:30 +0300

    sendmailer integration
    sendmailer integration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,12 +9,20 @@ const {validateBody, authenticate, upload} = require("../../middlewares");
 const {schemas} = require("../../models/user");
 
 
+// Sign Up
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.get("/verify/:verificationCode", ctrl.register);
+
+router.post("/verify", validateBody(schemas.userEmailSchema),);
+
+// Sign In
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.post("/logout", authenticate, ctrl.logout);
 
-router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
